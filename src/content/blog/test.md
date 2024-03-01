a8e9f8f6edd648abdb2999a422f41a013b8f7cb8
mostafaei2002 <sinamostafaei2002@gmail.com>
2024-03-01 17:26:27 +0330

    add simple blog

---
+++ src/content/blog/test.md
@@ -0,0 +1,8 @@
+---
+title: test blog post
+created_at: 2024-03-01
+updated_at: 2024-03-01
+summary: >
+  This blog post is only for testing purposes ignore
+publish: true
+---
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,31 @@
 import { z, defineCollection } from "astro:content";
 
 const projectsCollection = defineCollection({
-    type: "content",
-    schema: z.object({
-        title: z.string(),
-        date: z.date(),
-        image: z.string(),
-        repository: z.string(),
-        demo: z.string().optional(),
-        tags: z.array(z.string()),
-        description: z.string(),
-        score: z.number(),
-        publish: z.boolean(),
-    }),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    image: z.string(),
+    repository: z.string(),
+    demo: z.string().optional(),
+    tags: z.array(z.string()),
+    description: z.string(),
+    score: z.number(),
+    publish: z.boolean(),
+  }),
+});
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    created_at: z.date(),
+    updated_at: z.date(),
+    summary: z.string(),
+    publish: z.boolean(),
+  }),
 });
 
 export const collections = {
-    projects: projectsCollection,
+  projects: projectsCollection,
+  blog: blogCollection,
 };
